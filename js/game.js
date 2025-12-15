eaafbf99da32b0de72a782830da248af2c26a8d7
Arshya zarnegar <arshiya232354@gmail.com>
2025-12-15 09:41:19 +0330

    de.bugging

--- js/game.js
+++ js/game.js
@@ -3,41 +3,69 @@ import formatData from "./helper.js";
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
-const answerText = document.getElementById(".answer-text")
+const answerButtons = document.querySelectorAll(".answer-text");
 
 const URL = "https://opentdb.com/api.php?amount=10&type=multiple";
-let formattedData = null;
+
+let formattedData = [];
 let questionIndex = 0;
 let correctAnswer = null;
 
+
 const fetchData = async () => {
-  const response = await fetch(URL);
-  const json = await response.json();
-  console.log(json);
+  try {
+    const response = await fetch(URL);
+    const json = await response.json();
 
-  formattedData = formatData(json.results);
-  console.log(formattedData);
-
-  start();
+    formattedData = formatData(json.results);
+    start();
+  } catch (err) {
+    console.error("error", err);
+  }
 };
 
 
 const start = () => {
-  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
+  showQuestion();
 };
+
 
 const showQuestion = () => {
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
 
-    correctAnswer=correctAnswerIndex;
-    questionText.innerText=question
-    answerText?.onbeforematch((button,index)=>
-    {
-      button.innerText=answers[index];
-    })
+  correctAnswer = correctAnswerIndex;
+
+  questionText.innerHTML = question;
+
+  answerButtons.forEach((button, index) => {
+    button.innerText = answers[index];
+    button.onclick = () => checkAnswer(index);
+  });
+};
+
+
+const checkAnswer = (index) => {
+  if (index === correctAnswer) {
+    alert("✅ right");
+  } else {
+    alert("❌ wrong");
+  }
+
+  nextQuestion();
+};
+
+
+const nextQuestion = () => {
+  questionIndex++;
+
+  if (questionIndex < formattedData.length) {
+    showQuestion();
+  } else {
+    alert("🎉 the end quiz");
+  }
 };
 
 window.addEventListener("load", fetchData);
