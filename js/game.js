77c75702e6dd30a0c128db978708ebacc4d1c036
Arshya zarnegar <arshiya232354@gmail.com>
2025-12-17 11:02:57 +0330

    quizimport formatData from ./helper.js;
    const loader = document.getElementById(loader);
    const container = document.getElementById(container);
    const questionText = document.getElementById(question-text);
    const answerButtons = document.querySelectorAll(.answer-text);
    const scoreText = document.getElementById(scores);
    
    const CORRECT_BONUS = 10;
    
    const URL = https://opentdb.com/api.php?amount=10

--- js/game.js
+++ js/game.js
@@ -4,14 +4,17 @@ const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerButtons = document.querySelectorAll(".answer-text");
+const scoreText = document.getElementById("scores");
 
+const CORRECT_BONUS = 10;
 
 const URL = "https://opentdb.com/api.php?amount=10&type=multiple";
-
 
 let formattedData = [];
 let questionIndex = 0;
 let correctAnswer = null;
+let score = 0;
+
 /////////////////////////////////////////////////////////////////////////////////////////
 
 const fetchData = async () => {
@@ -25,56 +28,50 @@ const fetchData = async () => {
     console.error("error", err);
   }
 };
+
 /////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
 
 const start = () => {
   loader.style.display = "none";
   container.style.display = "block";
   showQuestion();
 };
+
 /////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 const showQuestion = () => {
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
 
   correctAnswer = correctAnswerIndex;
-
   questionText.innerHTML = question;
 
   answerButtons.forEach((button, index) => {
     button.innerText = answers[index];
+    button.dataset.number = index;
     button.disabled = false;
     button.classList.remove("correct", "wrong");
-    button.onclick = () => checkAnswer(index);
+
+    button.onclick = () => checkAnswer(button.dataset.number);
   });
 };
 
-
-
-
 /////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-const checkAnswer = (index) => {
+const checkAnswer = (number) => {
   answerButtons.forEach((btn) => (btn.disabled = true));
 
-  if (index === correctAnswer) {
-    answerButtons[index].classList.add("correct");
-  } else {
-    answerButtons[index].classList.add("wrong");
+  const selectedAnswer = Number(number);
 
+  if (selectedAnswer === correctAnswer) {
+    answerButtons[selectedAnswer].classList.add("correct");
+
+    score += CORRECT_BONUS;
+    scoreText.innerText = score;
+  } else {
+    answerButtons[selectedAnswer].classList.add("wrong");
     answerButtons[correctAnswer].classList.add("correct");
   }
-
-
 
   setTimeout(() => {
     questionIndex++;
@@ -87,10 +84,6 @@ const checkAnswer = (index) => {
   }, 1500);
 };
 
-
-
 /////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 window.addEventListener("load", fetchData);
